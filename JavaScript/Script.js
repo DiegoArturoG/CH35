f9126f3899992e9138c41daac20086396e8ffb72
DiegoArturoG <diegoargr1311@gmail.com>
2023-12-08 07:52:19 -0600

    Repositorio actualizado

---
+++ JavaScript/Script.js
@@ -0,0 +1,30 @@
+//comentarios una sola linea 
+/*Comentarios multilinea*/
+
+//alert("Hola mundo");
+//console.log("este es un dato para la console");
+//var nombre =prompt("ingresa tu nombre");
+//alert("hola, "+ nombre + "Bienvenido a ch35");
+
+//var numero1 = parseInt(prompt("Ingresa el primer numero"));
+//var numero2 = parseInt(prompt("ingresa el segundo numero"));
+
+//console.log("suma:"+(numero1 + numero2));
+//console.log("resta:"+(numero1 - numero2));
+//console.log("multiplicación:"+(numero1 * numero2));
+//console.log("división:"+(numero1 / numero2));
+
+var edad = prompt("ingresa tu edad");
+
+if (edad >=18) {
+    alert("Adelante brow");
+}
+else {
+    alert("Bebe no pasas, sorry")
+}
+if (edad >=18 &&  edad <= 65){
+    alert ("Si pasas brow")
+}
+if (edad >65){
+    alert ("My brow, muy viejo pa")
+}
